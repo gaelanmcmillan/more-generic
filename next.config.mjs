e31b760a53c27faea6c33e5757d019242199397c
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2022-10-30 19:30:58 -0400

    update path for image loader

--- next.config.mjs
+++ next.config.mjs
@@ -20,7 +20,7 @@ export default withMDX({
   pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
   images: {
     loader: "akamai",
-    path: "",
+    path: "/more-generic",
   },
   basePath: "/more-generic",
   assetPrefix: "/more-generic"
