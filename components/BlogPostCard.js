ada8813e072b9f3dd85d1b40b5b72d481909dc3c
Gaelan McMillan <gaelan.mcmillan@gmail.com>
2023-01-24 22:32:05 -0500

    adding responsiveness to cards

--- components/BlogPostCard.js
+++ components/BlogPostCard.js
@@ -4,6 +4,8 @@ import MaskedImage from "./MaskedImage";
 const hoverTime = 0.8;
 
 const HoverThumbnail = styled.div`
+  // border: 1px solid black;
+
   transition: ${hoverTime}s ease all;
   width: 100%;
   height: 100%;
@@ -12,29 +14,29 @@ const HoverThumbnail = styled.div`
   z-index: 2;
   position: absolute;
   display: inline-block;
-`
+`;
 
 const Wrapper = styled.div`
+  // border: 1px solid blue;
+
   font-family: Inconsolata;
   position: relative;
 
-  > h1, h2, h3 {
+  > h1,
+  h2,
+  h3 {
     font-weight: bold;
   }
 
   display: flex;
   flex-direction: row;
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+
   margin-bottom: 2rem;
-  // white-space: pre;
 
-  // overflow: hidden;
-  
-  // width: ${props => props.width || "100%"};
   height: auto;
-  // border: 1px solid orange;
-
-
-
 
   // Hover properties
   transition: ${hoverTime}s ease all;
@@ -53,80 +55,88 @@ const Wrapper = styled.div`
       border-color: var(--link-blue);
     }
   }
-`
+`;
 
 const ThumbnailWrapper = styled.div`
-  height: 200px;
+  display: flex;
+  align-self: center;
+  height: 250px;
+
+  @media (max-width: 600px) {
+    width: 100%;
+    height: 100%;
+    flex: 2;
+    aspect-ratio: 1;
+  }
   aspect-ratio: 1;
 
-  display: flex;
   justify-content: center;
   align-items: center;
-  // float: left;
-`
-  
+`;
+
 const Thumbnail = styled.div`
+  border: 1px solid grey;
+
   border-radius: 0.5rem;
   border-width: 4px;
   border-style: solid;
-  // overflow: hidden;
 
-  width: ${props => props.width};
-  height: ${props => props.height};
-`
-  
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+`;
+
 const InfoWrapper = styled.div`
+  // border: 1px solid magenta;
+
   position: relative;
-  padding-left: 0.5rem;
-  
-  display: flex;
+
+  // Adding this two keep the content inside its parent...
+  overflow: hidden;
+
   // keep items vertically centered
+  display: flex;
   align-items: center;
-
-  // max-width: inherit;
-  // height: inherit;
-  // vertical-align: top;
-
-  // white-space: normal;
-  // width: inherit;
-  // float: left;
-
-  // border: 1px solid green;
-`
+`;
 
 const InfoContent = styled.div`
+  // border: 1px solid green;
+
+  overflow: visible;
+
+  @media (max-width: 600px) {
+    margin-top: 0;
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+  }
+
   display: flex;
+  margin: 1rem;
   flex-direction: column;
-
-  height: 90%;
+  height: 95%;
   width: 95%;
-  // max-width: inherit;
+`;
 
-  // border: 1px solid red;
-`
-
-const Excerpt = styled.div`
-  display: flex;
-
-  // overflow: hidden;
-  // text-overflow: ellipsis;
-`
+const Excerpt = styled.div``;
 
 const DateAndAuthor = styled.span`
   margin-top: auto;
   align-self: start;
-`
+`;
 
-
-const BlogPostCard = ({slug, title, excerpt, thumbnail, date, author, prefix='/blog/'}) => {
+const BlogPostCard = ({
+  slug,
+  title,
+  excerpt,
+  thumbnail,
+  date,
+  author,
+  prefix = "/blog/",
+}) => {
   return (
-    <a style={{color: "unset", textDecoration: "none"}} href={prefix + slug}>
+    <a style={{ color: "unset", textDecoration: "none" }} href={prefix + slug}>
       <Wrapper className="soft-shadow soft-radius" width={"100%"}>
         <ThumbnailWrapper percentageOfWrapperWidth={"20"}>
-          <Thumbnail
-            width="90%"
-            height="90%"
-          >
+          <Thumbnail width="90%" height="90%">
             <MaskedImage
               width="100%"
               height="100%"
@@ -136,21 +146,34 @@ const BlogPostCard = ({slug, title, excerpt, thumbnail, date, author, prefix='/b
             >
               <HoverThumbnail></HoverThumbnail>
             </MaskedImage>
-
           </Thumbnail>
         </ThumbnailWrapper>
         <InfoWrapper width={"inherit"}>
           <InfoContent>
             <h2>{title}</h2>
             <Excerpt>{excerpt}</Excerpt>
-            <DateAndAuthor>{author}{' • '}{date}</DateAndAuthor>
+            <DateAndAuthor>
+              {author}
+              {" • "}
+              {date}
+            </DateAndAuthor>
           </InfoContent>
         </InfoWrapper>
       </Wrapper>
     </a>
-  )
-}
+  );
+};
 
 export default BlogPostCard;
 
-export {hoverTime, HoverThumbnail, Wrapper, Thumbnail, ThumbnailWrapper, InfoWrapper, InfoContent, Excerpt, DateAndAuthor}
+export {
+  hoverTime,
+  HoverThumbnail,
+  Wrapper,
+  Thumbnail,
+  ThumbnailWrapper,
+  InfoWrapper,
+  InfoContent,
+  Excerpt,
+  DateAndAuthor,
+};
--- components/SolutionCard.js
+++ components/SolutionCard.js
@@ -1,5 +1,4 @@
-import {InfoContent, Excerpt, DateAndAuthor} from "./BlogPostCard";
-import styled from 'styled-components';
+import styled from "styled-components";
 import TagBubble from "./TagBubble";
 /**
  * _____________________________________
@@ -9,31 +8,35 @@ import TagBubble from "./TagBubble";
  */
 
 const Wrapper = styled.div`
-  font-family: 'Inconsolata';
+  font-family: "Inconsolata";
 
   display: flex;
-  flex-direction: col;
+  flex-direction: row;
 
   margin-bottom: 1rem;
-  white-space: pre;
 `;
 
 const InfoWrapper = styled.div`
   width: 100%;
   height: 100%;
-  padding: 0.5rem;
-`
-
-const HorizontalSplit = styled.div`
   display: flex;
   flex-direction: row;
-`
+  padding: 0.5rem;
+`;
+
+const HorizontalSplit = styled.div`
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+`;
 
 const Title = styled.span`
   font-size: 18px;
-`
+`;
 
-
+const InfoContent = styled.div`
+  width: 100%;
+`;
 
 const extractSiteName = (url) => {
   const tokens = url.split(/\/+/);
@@ -41,43 +44,57 @@ const extractSiteName = (url) => {
     if (tok.endsWith(".com")) return tok.slice(0, tok.search(/.com/));
   }
   return "Link";
-}
+};
 
-const SolutionsCard = ({slug, title, url, tags, languages, date, prefix='/solutions/'}) => {
+const SolutionsCard = ({
+  slug,
+  title,
+  url,
+  tags,
+  languages,
+  date,
+  prefix = "/solutions/",
+}) => {
   const siteName = extractSiteName(url);
   return (
     <Wrapper className="soft-shadow" width={"100%"}>
-        <InfoWrapper width={"100%"}>
-          <HorizontalSplit style={{
-            }}>
-            <InfoContent style={{
-              }}>
-              <a style={{color: "unset", textDecoration: "none"}} href={prefix + slug}>
-                <Title>{title}</Title>
-                <HorizontalSplit>
-                  <HorizontalSplit>
-                    {tags.map((word, i) => {
-                      return <TagBubble key={i} tag={word} />
-                    })}
-                  </HorizontalSplit>
-                  <HorizontalSplit>
-                    {languages.map((lang, i) => {
-                      return <TagBubble key={i} tag={lang} />
-                    })}
-                  </HorizontalSplit>
-                </HorizontalSplit>
-              </a>
-            </InfoContent>
-            <InfoContent style={{
-              width: "25%", 
-              alignSelf: "center",
-              }}>
-              <a href={url}>{siteName} ⧉</a>
-            </InfoContent>
-          </HorizontalSplit>
-        </InfoWrapper>
-      </Wrapper>
+      <InfoWrapper width={"100%"}>
+        <InfoContent style={{}}>
+          <a
+            style={{ color: "unset", textDecoration: "none" }}
+            href={prefix + slug}
+          >
+            <Title>{title}</Title>
+            <div>
+              {tags.map((word, i) => {
+                return <TagBubble key={i} tag={word} />;
+              })}
+              {languages.map((lang, i) => {
+                return <TagBubble key={i} tag={lang} />;
+              })}
+            </div>
+          </a>
+        </InfoContent>
+        <div
+          style={{
+            float: "right",
+            width: "fit-content",
+            display: "flex",
+          }}
+        >
+          <div
+            style={{
+              margin: "auto",
+              whiteSpace: "pre",
+              margin: "auto",
+            }}
+          >
+            <a href={url}>{siteName} ⧉</a>
+          </div>
+        </div>
+      </InfoWrapper>
+    </Wrapper>
   );
-}
+};
 
 export default SolutionsCard;
