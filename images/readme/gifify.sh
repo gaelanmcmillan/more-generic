e026f4842b846a503e4560424515a0460a5290a9
gaelanmcmillan <73502924+gaelanmcmillan@users.noreply.github.com>
2023-02-11 18:39:26 +0000

    Deploying to gh-pages from @ gaelanmcmillan/more-generic@09f648e19cda3a686967b8f24ed11168998fe433 🚀

--- images/readme/gifify.sh
+++ images/readme/gifify.sh
@@ -3,5 +3,5 @@
 # source: https://superuser.com/questions/556029/how-do-i-convert-a-video-to-gif-using-ffmpeg-with-reasonable-quality
 
 ffmpeg -i $1.mov \
-    -vf "fps=24,scale=960:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
+    -vf "fps=24,scale=$2:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
     -loop 0 $1.gif
